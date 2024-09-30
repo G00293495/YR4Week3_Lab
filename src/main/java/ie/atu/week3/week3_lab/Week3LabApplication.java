70df2542f1c6aa3510c0035d1c0042b7309a061b
G00392495@atu.ie <G00392495@atu.ie>
2024-09-30 16:59:53 +0100

    added open feign

--- src/main/java/ie/atu/week3/week3_lab/Week3LabApplication.java
+++ src/main/java/ie/atu/week3/week3_lab/Week3LabApplication.java
@@ -2,8 +2,12 @@ package ie.atu.week3.week3_lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+
+
 public class Week3LabApplication {
 
     public static void main(String[] args) {
