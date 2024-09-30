60545c22b6273e4ed06019393421bfa41610c2fc
G00392495@atu.ie <G00392495@atu.ie>
2024-09-30 16:30:38 +0100

    project created

---
+++ src/test/java/ie/atu/week3/week3_lab/Week3LabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3.week3_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
